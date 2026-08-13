36cc3ef52b09c60f03f8824a6a87c6f91cf45801
katayan3290 <g27d43f8723@gmail.com>
2026-08-14 02:56:52 +0900

    Update test case to check EvenOrOdd with 8

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
 
-    result := EvenOrOdd(10)
+    result := EvenOrOdd(8)
     if result != "even" {
         t.Errorif("expected: even, actual: %s", result)
     }
